d59027bedfc0ebfe4775a9303ee77152760f2443
jirakit <pansapook@gmail.com>
2017-06-16 12:16:23 +0700

    project phonenumber

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Role;
+use App\Permission;
+use DB;
+
+class RoleController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $title = 'จัดการข้อมูลผู้ใช้';
+        $roles = Role::orderBy('id','DESC')->paginate(5);
+        return view('roles.index',compact('roles','title'))
+            ->with('i', ($request->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $title = 'จัดการข้อมูลผู้ใช้';
+        $permission = Permission::get();
+        return view('roles.create',compact('permission','title'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:roles,name',
+            'display_name' => 'required',
+            'description' => 'required',
+            'permission' => 'required',
+        ]);
+
+        $role = new Role();
+        $role->name = $request->input('name');
+        $role->display_name = $request->input('display_name');
+        $role->description = $request->input('description');
+        $role->save();
+
+        foreach ($request->input('permission') as $key => $value) {
+            $role->attachPermission($value);
+        }
+
+        return redirect()->route('roles.index')
+                        ->with('success','บันทึกข้อมูลเรียบร้อย');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $title = 'จัดการข้อมูลผู้ใช้';
+        $role = Role::find($id);
+        $rolePermissions = Permission::join("permission_role","permission_role.permission_id","=","permissions.id")
+            ->where("permission_role.role_id",$id)
+            ->get();
+
+        return view('roles.show',compact('role','rolePermissions','title'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $title = 'จัดการข้อมูลผู้ใช้';
+        $role = Role::find($id);
+        $permission = Permission::get();
+        $rolePermissions = DB::table("permission_role")->where("permission_role.role_id",$id)
+            ->pluck('permission_role.permission_id','permission_role.permission_id')->toArray();
+
+        return view('roles.edit',compact('role','permission','rolePermissions','title'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'display_name' => 'required',
+            'description' => 'required',
+            'permission' => 'required',
+        ]);
+
+        $role = Role::find($id);
+        $role->display_name = $request->input('display_name');
+        $role->description = $request->input('description');
+        $role->save();
+
+        DB::table("permission_role")->where("permission_role.role_id",$id)
+            ->delete();
+
+        foreach ($request->input('permission') as $key => $value) {
+            $role->attachPermission($value);
+        }
+
+        return redirect()->route('roles.index')
+                        ->with('success','อัพเดทข้อมูลเรียบร้อย');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        DB::table("roles")->where('id',$id)->delete();
+        return redirect()->route('roles.index')
+                        ->with('success','ลบข้อมูลเรียบร้อย');
+    }
+}
